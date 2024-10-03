dbe471e0c1a9e76f4da59d10b9f922fec65ef9ef
snehalavinashpatil <snehal141194@gmail.com>
2024-10-03 08:38:20 +0530

    episode 11

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const requestRouter = express.Router();
 const User = require("../models/user");
+const connectionRequest = require("../models/connectionRequest");
+
+const {userAuthentication} = require("../middleware/auth");
 
 //find user by email
 requestRouter.get("/User",async (req,res)=>{
@@ -39,5 +42,46 @@ requestRouter.patch("/user/:userId",async (req,res) =>{
       res.send(400).send("Update failed"+err.message);
     }
 });
+
+requestRouter.post("/request/send/:status/:toUserId",userAuthentication,async (req,res)=>{
+try{
+  const fromUserId = req.user._id;
+  const toUserId = req.params.toUserId;
+  const status = req.params.status;
+
+  //interested or ignored
+  const allowedStatus = ["interested","ignored"];
+  if(!allowedStatus.includes(status)){
+    return res.status(400).json({message:"invalid status"+status});
+  }
+
+  //check if their is existing/pending connection request in schema 
+  const existingConnectionRequest = await connectionRequest.findOne({
+    $or:[{fromUserId,toUserId},{fromUserId:toUserId,toUserId:fromUserId}]
+  });
+
+  if(existingConnectionRequest){
+    return res.status(400).send("connection request already exists");
+  }
+
+  //check if use id is invalid
+  const toUser = await User.findById(toUserId);
+  console.log(toUser,'toUser');
+  if(!toUser){
+    res.status(404).json({message:"user not found"+status});
+  }
+
+  const request = new connectionRequest({fromUserId,toUserId,status});
+
+  const data = await request.save();
+
+  res.json({
+    message:"request sent successfully",data
+  });
+ // res.send("User Updated Successfuly");
+}catch(err){
+  res.status(400).send("request failed"+err.message);
+}
+})
 
 module.exports = requestRouter;
